5ef8dd92817fc9223c8535b049de063c33015943
PhileloMphaga <Philelo.Mphaga@ilabqa.com>
2025-09-09 16:46:08 +0200

    Chapter 2 operators covered

---
+++ JavaFundamentalsTraining/src/Chapter2/operators/RelationalOperators.java
@@ -0,0 +1,21 @@
+package Chapter2.operators;
+
+/**
+ * @author Philelo Mphaga
+ * this class shows different relational operators
+ * explore relationship between operand
+ */
+public class RelationalOperators {
+    public static void main(String[] args) {
+
+        double operand1 = 7;
+        double operand2 = 12;
+
+        System.out.println("1 = " + (operand1 == operand2));
+        System.out.println("2 = " + (operand1 != operand2));
+        System.out.println("3 = " + (operand1 > operand2));
+        System.out.println("4 = " + (operand1 < operand2));
+        System.out.println("5 = " + (operand1 >= operand2));
+        System.out.println("6 = " + (operand1 <= operand2));
+    }
+}
